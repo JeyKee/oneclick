c6acbe378baf919ee649ecce36de29e5e699dda2
JK <torkotukzena@gmail.com>
2017-11-05 12:01:25 +0200

    onclick

---
+++ index.js
@@ -0,0 +1,10 @@
+var likes1 = 0 ;
+function click1(){
+    likes1 = likes1 + 1 ;
+    console.log("Image one likes:" + likes1);
+}   
+var dislikes = 0 ;
+function click2(){
+    dislikes = dislikes + 1 ;
+    console.log ("Image one disleks:" + dislikes);
+}
